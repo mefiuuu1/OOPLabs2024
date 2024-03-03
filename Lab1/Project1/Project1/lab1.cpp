0700465d0f52b1a87175fd2a6654f27474da3bd3
mefiuuu1 <145164758+mefiuuu1@users.noreply.github.com>
2024-03-03 20:03:51 +0200

    lab1.cpp

--- Lab1/Project1/Project1/lab1.cpp
+++ Lab1/Project1/Project1/lab1.cpp
@@ -7,13 +7,16 @@ int stringToInt(char* str) {
 	return rez;
 }
 int main() {
-	FILE* file = fopen("in.txt", "r");
-	char text[256];
-	int s = 0;
-	while (fgets(text, sizeof(text), file))
-		s = stringToInt(text);
-	printf("suma numerelor este %d\n", s);
-	fclose(file);
-	return 0;
-
+    FILE* file;
+    if (fopen_s(&file, "in.txt", "r") != 0) {
+        printf("Eroare la deschiderea fisierului!");
+        return 1;
+    }
+    char text[256];
+    int s = 0;
+    while (fgets(text, sizeof(text), file))
+        s += stringToInt(text);
+    printf("Suma numerelor este %d\n", s);
+    fclose(file);
+    return 0;
 }
